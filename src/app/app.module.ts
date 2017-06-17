29888e694b4fabeefab552a7ded4ca35f216a964
Ben Petrosky <benpetrosky@hotmail.com>
2017-06-17 12:49:10 -0700

    getting through the auth0 walkthrough 29:35

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import {routing, appRoutingProviders} from './app.routing';
+import {AuthService} from './auth.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [appRoutingProviders, AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
