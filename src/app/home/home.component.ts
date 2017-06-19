0766a83115d65e793049b734e118988a51586fec
Ben Petrosky and Jesse Hoare Evans <ben+jesse@>
2017-06-19 11:45:49 -0700

    add dynamic login UI and logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,23 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  profile:any;
+
   constructor(private auth: AuthService){
 
   }
 
   ngOnInit() {
+    if (this.auth.userProfile) {
+   this.profile = null;
+
+ } else {
+   this.auth.getProfile((err, profile) => {
+     this.profile = profile;
+     console.log(this.profile)
+   });
+ }
+
   }
 
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,18 +13,22 @@ export class ProfileComponent implements OnInit {
   profile: any;
 
   constructor(private auth: AuthService){
+
+
     // this.profile = JSON.parse(localStorage.getItem('profile'));
     // console.log(this.profile);
   }
 
   ngOnInit() {
-    if (this.auth.userProfile) {
-   this.profile = this.auth.userProfile;
- } else {
-   this.auth.getProfile((err, profile) => {
-     this.profile = profile;
-   });
- }
+      if (this.auth.userProfile) {
+     this.profile = this.auth.userProfile;
+
+   } else {
+     this.auth.getProfile((err, profile) => {
+       this.profile = profile;
+       console.log(this.profile)
+     });
+   }
   }
 
 }
